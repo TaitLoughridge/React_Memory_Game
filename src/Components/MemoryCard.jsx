6c6ab86477b9f0ce5d5b0ba6ebfd6d0145bee8c1
Tait Loughridge <68408313+TaitLoughridge@users.noreply.github.com>
2020-10-07 11:01:04 -0500

    part one

---
+++ src/Components/MemoryCard.jsx
@@ -0,0 +1,14 @@
+
+import React, { component } from 'react';
+import './MemoryCard.css';
+
+function MemoryCard(props) {
+    return(
+        <div className='MemoryCard'>
+            {/* <img src="https://www.digitalcrafts.com/img/digitalcrafts-logo-white-y.png"/> */}
+        </div>
+    )
+};
+
+
+export default MemoryCard;
