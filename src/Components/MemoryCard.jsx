f4cd4b0090155bc9d98967cc930b229d2f51a1e6
Tait Loughridge <68408313+TaitLoughridge@users.noreply.github.com>
2020-10-07 15:34:56 -0500

    step-2 card not flipping yet

--- src/Components/MemoryCard.jsx
+++ src/Components/MemoryCard.jsx
@@ -1,18 +1,36 @@
 
-import React, { component } from 'react';
+import React, { Component } from 'react';
 import './MemoryCard.css';
 
-function MemoryCard(props) {
-    return(
-        <div className='MemoryCard'>
-            <div className='MemoryCardInner'>
-                <div className='MemoryCardBack'>
-                    <img src="https://www.digitalcrafts.com/img/digitalcrafts-logo-white-y.png"/>
+class MemoryCard extends Component {
+
+    constructor(props) {
+        super(props)
+        this.state = {isFlipped: false};
+    }
+    clickHandler() {
+        this.setState({
+            isFlipped: !this.state.isFlipped
+        })
+    }
+
+    render() {
+        let MemoryCardInnerClass = 'MemoryCardInner'
+        if (this.props.isFlipped) {
+            MemoryCardInnerClass += 'flipped'
+        }
+    
+        return(
+            <div className='MemoryCard' onClick={this.clickHandler.bind(this)}>
+                <div className={MemoryCardInnerClass}>
+                    <div className='MemoryCardBack'>
+                        <img src="https://www.digitalcrafts.com/img/digitalcrafts-logo-white-y.png"/>
+                    </div>
+                    <div className='MemoryCardFront'>▲</div>
                 </div>
-                <div className='MemoryCardFront'>▲</div>
             </div>
-        </div>
-    )
+        )
+    }
 };
 
 
